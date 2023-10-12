4b3e7ec05ab2f421836da3cce065f4c08e3c8280
maxiberosich <mberosich@gmail.com>
2023-10-11 22:15:30 -0300

    Vinculacion imagen con usuario

--- Biblioteca/src/main/java/com/equipo52/biblioteca/servicios/ImagenServicio.java
+++ Biblioteca/src/main/java/com/equipo52/biblioteca/servicios/ImagenServicio.java
@@ -5,7 +5,9 @@
 package com.equipo52.biblioteca.servicios;
 
 import com.equipo52.biblioteca.entidades.Imagen;
+import com.equipo52.biblioteca.excepciones.MiExcepcion;
 import com.equipo52.biblioteca.repositorios.ImagenRepositorio;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,18 +18,53 @@ import org.springframework.web.multipart.MultipartFile;
  */
 @Service
 public class ImagenServicio {
-    
+
     @Autowired
     private ImagenRepositorio imagenRepositorio;
-    
-    public Imagen guardar(MultipartFile archivo){
-        if(archivo != null){
-            
-            Imagen imagen = new Imagen();
-            
-            imagen.
-            
+
+    public Imagen guardar(MultipartFile archivo) throws MiExcepcion {
+        if (archivo != null) {
+            try {
+                Imagen imagen = new Imagen();
+
+                imagen.setMime(archivo.getContentType());
+
+                imagen.setNombre(archivo.getName());
+
+                imagen.setContenido(archivo.getBytes());
+
+                return imagenRepositorio.save(imagen);
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+            }
         }
+        return null;
     }
-    
+
+    public Imagen actualizar(MultipartFile archivo, String idImagen) throws MiExcepcion {
+        if (archivo != null) {
+            try {
+                Imagen imagen = new Imagen();
+
+                if (idImagen != null) {
+
+                    Optional<Imagen> respuesta = imagenRepositorio.findById(idImagen);
+
+                    if (respuesta.isPresent()) {
+                        imagen = respuesta.get();
+                    }
+                }
+                
+                imagen.setMime(archivo.getContentType());
+                imagen.setNombre(archivo.getName());
+                imagen.setContenido(archivo.getBytes());
+                
+                return imagenRepositorio.save(imagen);
+            } catch (Exception ex) {
+                System.err.println(ex.getMessage());
+            }
+        }
+        return null;
+    }
+
 }
--- Biblioteca/src/main/java/com/equipo52/biblioteca/servicios/UsuarioServicio.java
+++ Biblioteca/src/main/java/com/equipo52/biblioteca/servicios/UsuarioServicio.java
@@ -4,6 +4,7 @@
  */
 package com.equipo52.biblioteca.servicios;
 
+import com.equipo52.biblioteca.entidades.Imagen;
 import com.equipo52.biblioteca.entidades.Usuario;
 import com.equipo52.biblioteca.enumeraciones.Rol;
 import com.equipo52.biblioteca.excepciones.MiExcepcion;
@@ -12,6 +13,7 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -23,73 +25,111 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
  * @author Max
  */
 @Service
-public class UsuarioServicio implements UserDetailsService{
-    
+public class UsuarioServicio implements UserDetailsService {
+
     @Autowired
     private UsuarioRepositorio usuarioRepositorio;
-    
+
+    @Autowired
+    private ImagenServicio imagenServicio;
+
     @Transactional
-    public void registrar(String nombre, String email, String password, String password2) throws MiExcepcion{
-        
+    public void registrar(MultipartFile archivo, String nombre, String email, String password, String password2) throws MiExcepcion {
+
         validar(nombre, email, password, password2);
-        
+
         Usuario usuario = new Usuario();
-        
+
         usuario.setNombre(nombre);
         usuario.setEmail(email);
-        
+
         usuario.setPassword(new BCryptPasswordEncoder().encode(password));
-        
+
         usuario.setRol(Rol.USER);
-        
+
+        Imagen imagen = imagenServicio.guardar(archivo);
+
+        usuario.setImagen(imagen);
+
         usuarioRepositorio.save(usuario);
-        
+
     }
-    
-    private void validar(String nombre, String email, String password, String password2) throws MiExcepcion{
-        
-        if(nombre.isEmpty() || nombre == null){
+
+    @Transactional
+    public void actualizar(MultipartFile archivo, String idUsuario, String nombre, String email, String password, String password2) throws MiExcepcion {
+
+        validar(nombre, email, password, password2);
+
+        Optional<Usuario> respuesta = usuarioRepositorio.findById(idUsuario);
+        if (respuesta.isPresent()) {
+            Usuario usuario = respuesta.get();
+
+            usuario.setNombre(nombre);
+            usuario.setEmail(email);
+            usuario.setPassword(new BCryptPasswordEncoder().encode(password));
+
+            usuario.setRol(Rol.USER);
+
+            String idImagen = null;
+            
+            if(usuario.getImagen() != null){
+                idImagen = usuario.getImagen().getId();
+            }
+            
+            Imagen imagen = imagenServicio.actualizar(archivo,idImagen);
+
+            usuario.setImagen(imagen);
+
+            usuarioRepositorio.save(usuario);
+        }
+
+    }
+
+    private void validar(String nombre, String email, String password, String password2) throws MiExcepcion {
+
+        if (nombre.isEmpty() || nombre == null) {
             throw new MiExcepcion("El nombre no puede ser nulo o estar vacio");
         }
-        if(email.isEmpty() || email == null){
+        if (email.isEmpty() || email == null) {
             throw new MiExcepcion("El email no puede ser nulo o estar vacio");
         }
-        if(password.isEmpty() || password == null || password.length() < 4){
+        if (password.isEmpty() || password == null || password.length() < 4) {
             throw new MiExcepcion("La contraseña no puede estar vacia, y debe tener mas de 3 caracteres");
         }
-        if(!password.equals(password2)){
+        if (!password.equals(password2)) {
             throw new MiExcepcion("Las contraseñas ingresadas deben ser iguales");
         }
-        
+
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Usuario usuario = usuarioRepositorio.buscarPorEmail(email);
-        
-        if(usuario != null){
+
+        if (usuario != null) {
             List<GrantedAuthority> permisos = new ArrayList();
-            
+
             GrantedAuthority p = new SimpleGrantedAuthority("ROLE_" + usuario.getRol().toString());
-            
+
             permisos.add(p);
-            
+
             ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
-            
+
             HttpSession session = attr.getRequest().getSession(true);
-            
+
             session.setAttribute("usuariosession", usuario);
-            
-            return new User(usuario.getEmail(), usuario.getPassword(),permisos);
-        }else{
+
+            return new User(usuario.getEmail(), usuario.getPassword(), permisos);
+        } else {
             return null;
         }
     }
-    
+
 }
